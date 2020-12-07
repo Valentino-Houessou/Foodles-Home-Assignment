63afd275c684c8fa089e8aec6b756505eb47271c
Valentino-Houessou <valentino.houessou@gmail.com>
2020-12-07 02:21:19 +0100

    Serve images as static file

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import "reflect-metadata";
 import config from "../config/common";
 import { createSchema } from "./utils/createSchema";
 import { openDBConnection } from "./utils/database";
+import path from "path";
 
 const main = async () => {
   const conn = await openDBConnection();
@@ -11,11 +12,15 @@ const main = async () => {
   conn.runMigrations();
 
   const app = express();
+
   const apolloServer = new ApolloServer({
     schema: await createSchema(),
   });
 
   apolloServer.applyMiddleware({ app });
+
+  // to server static files
+  app.use(express.static(path.join(__dirname, "../public")));
 
   app.listen(config.port, () => {
     console.log(`server started on port ${config.port}`);
