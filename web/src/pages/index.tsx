15c0b217c9ae6e48a1911fba59b72fb2d6d2badf
Valentino-Houessou <valentino.houessou@gmail.com>
2020-12-07 16:47:14 +0100

    Set up front

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+const Index = () => (
+  <Box bgColor="#f0f2f2" position="fixed" top="0" bottom="0" right="0" left="0">
+    Hello Word
+  </Box>
+);
+
+export default Index;
