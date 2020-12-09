b1b1a57b1284564320471d21eaebf9f6ed24e885
Valentino-Houessou <valentino.houessou@gmail.com>
2020-12-09 12:28:46 +0100

    Update Index

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import { NavBar } from "../components/NavBar";
+import { useUser } from "../components/UserProvider";
 
-const Index = () => (
-  <Box bgColor="#f0f2f2" position="fixed" top="0" bottom="0" right="0" left="0">
-    Hello Word
-  </Box>
-);
+const Index = () => {
+  const user = useUser();
+
+  return (
+    <Box bg="#f0f2f2" position="fixed" top="0" bottom="0" right="0" left="0">
+      <NavBar />
+      <Box m={5}>
+        <Text fontSize="1.6em" fontWeight="bold" my={14}>
+          Livraison
+        </Text>
+        <Box>List of product</Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default Index;
