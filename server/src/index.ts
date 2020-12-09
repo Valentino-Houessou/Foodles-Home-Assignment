a120a4a6497fd9ec1ddab2eb749f0e086b4a58fa
Valentino-Houessou <valentino.houessou@gmail.com>
2020-12-09 12:24:05 +0100

    Add cors to the back end

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
 import express from "express";
+import path from "path";
 import "reflect-metadata";
 import config from "../config/common";
 import { createSchema } from "./utils/createSchema";
 import { openDBConnection } from "./utils/database";
-import path from "path";
 
 const main = async () => {
   const conn = await openDBConnection();
@@ -13,11 +14,14 @@ const main = async () => {
 
   const app = express();
 
+  //set up cors with express cors middleware
+  app.use(cors({ origin: "http://localhost:3000" }));
+
   const apolloServer = new ApolloServer({
     schema: await createSchema(),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
   // to server static files
   app.use(express.static(path.join(__dirname, "../public")));
