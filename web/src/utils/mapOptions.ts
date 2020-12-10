b92c3f874e8c90a4f2263120e42f65c1a71a2359
Valentino-Houessou <valentino.houessou@gmail.com>
2020-12-10 12:45:34 +0100

    Fixed client credit

--- web/src/utils/mapOptions.ts
+++ web/src/utils/mapOptions.ts
@@ -3,6 +3,6 @@ import { Client } from "../generated/graphql";
 export const mapOptions = (searchResult: Client[]) => {
   return searchResult.map((client: Client) => ({
     value: client.id,
-    label: `${client.email} // ${client.credit}€`,
+    label: `${client.email} // ${client.credit.toFixed(2)}€`,
   }));
 };
